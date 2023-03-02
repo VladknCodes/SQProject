386fb1f547e407d07528c3e609bf09ad9c177127
Vladislav Kartashov <61541211+VladknCodes@users.noreply.github.com>
2023-03-02 20:47:47 +0300

    Update admin.py

--- Supplier_Qualification/admin.py
+++ Supplier_Qualification/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Person, Supplier, DRA, News
+from .models import Person, Supplier, DRA, News, Product
 
 admin.site.register(Person)
 admin.site.register(Supplier)
 admin.site.register(DRA)
 admin.site.register(News)
+admin.site.register(Product)
